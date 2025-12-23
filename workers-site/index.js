e5c990bbbf4971ef39d1a28978ebf0ce8cfff61c
Tom6255 <110585111+Tom6255@users.noreply.github.com>
2025-12-23 09:39:48 +0800

    Delete workers-site directory

--- workers-site/index.js
+++
@@ -1,7 +0,0 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
-
-export default {
-  async fetch(request, env, ctx) {
-    return getAssetFromKV({ request, waitUntil: ctx.waitUntil })
-  }
-}
